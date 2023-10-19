e6d72bd8e873f6e65a29fa54b1da243138c0e80f
Saksham <saksham.27032002@gmail.com>
2023-10-19 13:48:05 +0530

    layout design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import {apiUrl,filterData} from "./data";
+import Navbar from "./Components/Navbar";
+import Filter from "./Components/Filter";
+import Cards from "./Components/Cards";
+import { toast } from "react-toastify";
 const App = () => {
-  return <div>App</div>;
+   const [courses,setCourses]=useState(null);
+  useEffect(()=>{
+    const fetchData =async()=>{
+      try {
+        const res=await fetch(apiUrl);
+        const output=await res.json();
+        //save data into a variable 
+        setCourses(output.data);
+      } catch (error) {
+        toast.error("Something went wrong");
+      }
+    }
+    fetchData();
+  },[])
+  return (
+    <div>
+      <Navbar/>
+      <Filter filterData={filterData}/>
+      <Cards courses={courses}/>
+    </div>
+  )
 };
 
 export default App;
