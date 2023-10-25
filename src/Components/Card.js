b77e826b1a2d6745979a4683c8fe710599a3cea2
Saksham <saksham.27032002@gmail.com>
2023-10-25 10:52:36 +0530

    Liked Unliked feature added/toast

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { FcLike } from 'react-icons/fc';
+import { toast } from 'react-toastify';
 
 const Card = (props) => {
   let course = props.course;
+  let likedCourses=props.likedCourses;
+  let setLikedCourses=props.setLikedCourses;
+  function clickHandler()
+  {
+    if(likedCourses.includes(course.id)) 
+    {
+      //already liked
+      setLikedCourses((prev)=>prev.filter((cid)=>(cid !=course.id)));
+      toast.warning("Like removed")
+    }
+    else{
+      //phela sa like nhi ha ya course
+      //insert karna h ye course likedCourses mein
+       if(likedCourses.length === 0)
+       {
+         setLikedCourses([course.id]);
+       }
+       else
+       {
+        //non-empty phela sa
+         setLikedCourses((prev)=>[...prev,course.id]);
+       }
+       toast.success("Liked Successfully");
+    }
+  }
   return (
     <div className='w-[300px] bg-bgDark bg-opacity-80 rounded-md overflow-hidden'>
       <div className='relative'>
         <img src={course.image.url} alt={course.title} />
 
         <div className='w-[40px] h-[40px] bg-white rounded-full absolute right-2 bottom-3 grid place-items-center'>
-          <button>
+          <button onClick={clickHandler}>
             <FcLike fontSize="1.75rem" />
           </button>
         </div>
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card';
 const Cards = (props) => {
     let courses=props.courses;
+    const[likedCourses,setLikedCourses]=useState([]);
     
     //returns you a list of all courses recieved from api response 
   function getCourses()
@@ -18,7 +19,7 @@ const Cards = (props) => {
   return (
     <div className='flex flex-wrap justify-center mb-4 gap-4'>
      {getCourses().map((course) => {
-       return <Card key={course.id} course={course}/>
+       return <Card key={course.id} course={course} likedCourses={likedCourses} setLikedCourses={setLikedCourses}/>
       })}
     </div>
   )
